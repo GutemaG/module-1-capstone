a324d9881329de6df6313d8afce30c12a0f8f7f8
GutemaG <bir13gud17@gmail.com>
2022-11-23 14:58:35 +0300

    Change all comments from code-reveiwer add video url in about page add parenter section

--- README.md
+++ README.md
@@ -71,7 +71,7 @@
 - [Live Demo Link](https://gutemag.github.io/module-1-capstone/)
 
 ## Video Description <a name="video-description"></a>
-- [Live Demo Link](https://gutemag.github.io/module-1-capstone/)
+- [Video Description](https://www.loom.com/share/0259b529b1184f48a7f0b3cd2c78d318)
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
 <!-- GETTING STARTED -->
--- js/about.js
+++ js/about.js
@@ -1,6 +1,7 @@
 const navMenu = document.getElementById('nav-menu-show');
 const navItems = document.querySelector('.nav-items');
 const closeMenuIcon = document.querySelector('.nav-close-icon');
+const partnersList = document.querySelector('#partners');
 
 navMenu.addEventListener('click', () => {
   navItems.classList.toggle('show');
@@ -11,3 +12,18 @@ closeMenuIcon.addEventListener('click', () => {
   navItems.classList.remove('show');
   navItems.classList.toggle('hide');
 });
+
+fetch('data.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const partnerData = data.partners;
+    for (let i = 0; i < partnerData.length; i += 1) {
+      const partnerListItem = document.createElement('li');
+      partnerListItem.innerHTML = `
+        <li class='partner'>
+            <img src='${partnerData[i].image}' alt='${partnerData[i].name}' />
+        </li>
+        `;
+      partnersList.append(partnerListItem);
+    }
+  });
